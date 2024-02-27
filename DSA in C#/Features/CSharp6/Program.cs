25ec8001d394bb93b3559fe7d6a26c0bd945837e
Raihan Mehran <raihan.mehran@hotmail.com>
2024-02-27 23:43:34 +0100

    Practicing C# Features

---
+++ DSA in C#/Features/CSharp6/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Features;
+
+class Student
+{
+    public int Id { get; set; }
+    public String Name { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello");
+
+        int[] arr = new int[5];
+
+        var arr2 = new int[5];
+
+        List<Student> studentList = new List<Student>();
+        List<int> numberList = new List<int>();
+        List<int> numberList2 = [];
+
+
+
+
+        Console.WriteLine("Hello, World!");
+    }
+}
